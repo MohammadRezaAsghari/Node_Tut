9a48b669fc73843ef240a6d0528458e3fb876672
MohammadRezaAsghari <mrezaasghari.mra@gmail.com>
2019-12-07 23:06:49 +0330

    make a simple api

--- 02-create-server/script.js
+++ 02-create-server/script.js
@@ -1,5 +1,7 @@
+const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
 
 const server = http.createServer((req , res)=>{
 
@@ -13,6 +15,14 @@ const server = http.createServer((req , res)=>{
     }
     else if(pathName === '/product'){
         res.end('We are in product page');
+    }
+    else if(pathName === '/api'){
+        fs.readFile(`${__dirname}/dev-data/data.json` , 'utf-8' , (err , data) =>{
+            const parsedData = JSON.parse(data);
+            res.writeHead(200 , {'Content-type' : 'application/json'});
+            res.end(data);
+        });
+        
     }
     else{
         res.writeHead(404 , {
